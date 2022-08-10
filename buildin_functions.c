f8d609c74fa2e3c53e9526d86aa1a0968a9683dd
Gmakaulule <giftmakaulule@gmail.com>
2022-08-10 09:46:02 -0700

    debug

--- buildin_functions.c
+++ buildin_functions.c
@@ -85,23 +85,3 @@ void (*check_built_ins(char *str))(char *str)
 	}
 	return (NULL);
 }
-
-/**
- * built_in - Checks for builtin functions.
- * @command: An array of all the arguments passed to the shell.
- * @line: A string representing the input from the user.
- * Return: If function is found 0. Otherwise -1.
- */
-int built_in(char **command, char *line)
-{
-	void (*build)(char *);
-
-	build = check_built_ins(command[0]);
-	if (build == NULL)
-		return (-1);
-	if (_strcmp("exit", command[0]) == 0)
-		double_free(command);
-	build(line);
-	return (0);
-}
-
