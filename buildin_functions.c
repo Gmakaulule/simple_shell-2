f7912f5733c06807a53d8fd4e6ba82020b17c5ee
Gmakaulule <giftmakaulule@gmail.com>
2022-08-10 07:01:50 -0700

    debug_buildin_functions.c

--- buildin_functions.c
+++ buildin_functions.c
@@ -8,28 +8,28 @@
 void _cd(char *line)
 {
 	int index;
-	int token_count;
-	char **param_array;
+	int token_num;
+	char **array;
 	const char *delim = "\n\t ";
 
-	token_count = 0;
-	param_array = token_interface(line, delim, token_count);
-	if (param_array[0] == NULL)
+	token_num = 0;
+	array = token_interface(line, delim, token_num);
+	if (array[0] == NULL)
 	{
-		single_free(2, param_array, line);
+		single_free(2, array, line);
 		return;
 	}
-	if (param_array[1] == NULL)
+	if (array[1] == NULL)
 	{
 		index = find_path("HOME");
 		chdir((environ[index]) + 5);
 	}
-	else if (_strcmp(param_array[1], "-") == 0)
-		print_str(param_array[1], 0);
+	else if (_strcmp(array[1], "-") == 0)
+		print_str(array[1], 0);
 
 	else
-		chdir(param_array[1]);
-	double_free(param_array);
+		chdir(array[1]);
+	double_free(array);
 }
 
 /**
